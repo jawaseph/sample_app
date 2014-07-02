786d40c6212ed97e7869da63ae698e216e8b92ca
Joe Calamia <joseph.calamia@yale.edu>
2014-07-02 14:30:54 -0400

    Revert "messed up spec"
    This reverts commit ec6ad533d31dc0b41cb4167260f250eaf777b199.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,76 +1,95 @@
+
+
+
 require 'spec_helper'
+
+# describe "StaticPages" do
+#   describe "GET /static_pages" do
+#     it "works! (now write some real specs)" do
+#       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+#       get static_pages_index_path
+#       response.status.should be(200)
+#     end
+#   end
+# end
 
 describe "Static pages" do
 
-  subject { page }
+	let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
-  describe "Home page" do
-    before { visit root_path }
+  	describe "Home page" do
+		context "my context", :type => :feature do	
+		    it "should have the content 'Sample App'" do
+		      visit root_path
+		      expect(page).to have_content( 'Sample App')
+		    end
 
-    it { should have_content('Sample App') }
-    it { should have_title(full_title('')) }
-    it { should_not have_title('| Home') }
-  
-    describe "for signed-in users" do
-      let(:user) { FactoryGirl.create(:user) }
-        before do
-          FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
-          FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
-          sign_in user
-          visit root_path
-        end
+		    # it "should have the title 'Home'" do
+		    #   visit '/static_pages/home'
+		    #   expect(page).to have_title( "#{base_title} | Home")
+		    # end
+		end	
+	end
 
-        it "should render the user's feed" do
-          user.feed.each do |item|
-            expect(page).to have_selector("li##{item.id}", text: item.content)
-          end
-        end
-        describe "follower/following counts" do
-          let(:other_user) { FactoryGirl.create(:user) }
-          before do
-            other_user.follow!(user)
-            visit root_path
-          end
+	describe "Help page" do
+		context "my context", :type => :feature do	
+		    it "should have the content 'Help'" do
+		      visit help_path
+		      expect(page).to have_content('Help')
+		    end
 
-          it { should have_link("0 following", href: following_user_path(user)) }
-          it { should have_link("1 followers", href: followers_user_path(user)) }
-        end
-      end
+		    it "should have the title 'Help'" do
+		      visit help_path
+		      expect(page).to have_title("#{base_title} | Help")
+		    end
+		end
+  	end
+
+  	describe "About page" do
+	  	context "my context", :type => :feature do
+		    it "should have the content 'About Us'" do
+		      visit about_path
+		      expect(page).to have_content('About Us')
+		    end
+
+		    it "should have the title 'About Us'" do
+		      visit about_path
+		      expect(page).to have_title("#{base_title} | About Us")
+		    end
+		end
+  	end
+
+  	  	describe "Contact page" do
+		context "my context", :type => :feature do	
+		    it "should have the content 'Contact'" do
+		      visit contact_path
+		      expect(page).to have_content('Contact')
+		    end
+
+		    it "should have the title 'Contact'" do
+		      visit contact_path
+		      expect(page).to have_title("#{base_title} | Contact")
+		    end
+		end	
+	end
+
+	describe "Home page" do
+	context "my context", :type => :feature do	
+    it "should have the content 'Sample App'" do
+      visit root_path
+      expect(page).to have_content('Sample App')
     end
 
-  describe "Help page" do
-    before { visit help_path }
+    it "should have the base title" do
+      visit root_path
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+    end
 
-    it { should have_content('Help') }
-    it { should have_title(full_title('Help')) }
+    it "should not have a custom page title" do
+      visit root_path
+      expect(page).not_to have_title('| Home')
+    end
+	end
   end
-
-  describe "About page" do
-    before { visit about_path }
-
-    it { should have_content('About') }
-    it { should have_title(full_title('About Us')) }
-  end
-
-  describe "Contact page" do
-    before { visit contact_path }
-
-    it { should have_content('Contact') }
-    it { should have_title(full_title('Contact')) }
-  end
-
-  it "should have the right links on the layout" do
-    visit root_path
-    click_link "About"
-    expect(page).to have_title(full_title('About Us'))
-    click_link "Help"
-    expect(page).to have_title(full_title('Help'))
-    click_link "Contact"
-    expect(page).to have_title(full_title('Contact'))
-    click_link "Home"
-    click_link "Sign up now!"
-    expect(page).to have_title(full_title('Sign up'))
-    click_link "sample app"
-    expect(page).to have_selector('h1', text: 'Welcome to the Sample App')
-  end
+  	
 end
