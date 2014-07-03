b109d3f79a22dbb8876d847ff727e0eea96f793a
Joe Calamia <joseph.calamia@yale.edu>
2014-07-03 14:46:46 -0400

    add user microposts

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,10 @@ require 'spec_helper'
 
 describe User do
 
-  before { @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar") }
+  before do
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar")
+  end
 
   subject { @user }
 
